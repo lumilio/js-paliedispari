232bdad743c2b12f8756961bda870a0483e936b2
lumilio <logistic-division@lumilium.com>
2021-10-20 01:46:31 +0200

    es parola palindroma no bonus

---
+++ parola palindroma/palindroma.js
@@ -0,0 +1,22 @@
+
+let parola = prompt('inserisci una parola');
+
+function verificaPalindromi() {
+
+    let parolaX = parola.split("").reverse().join("");
+
+    if(parola == parolaX){
+         alert('la tua parolla è palindroma')
+    }
+    else{
+         alert('la parola inserita non è palindroma')
+    }
+}
+
+verificaPalindromi();
+
+
+
+
+
+
